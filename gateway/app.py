3e282e639161b1ff4386d924c47d99488d108a46
Alberto Tomasin <alberto.tomasin@gmail.com>
2023-09-29 18:32:10 +0200

    added forecast to gateway

--- gateway/app.py
+++ gateway/app.py
@@ -5,15 +5,20 @@ import requests
 app = Flask(__name__)
 CORS(app)
 
-feed_service_url = "http://127.0.0.1:5000"
-
+feed_service_url = "http://127.0.0.1:1234"
+forecast_service_url = "http://127.0.0.1:1235"
 
 @app.route('/feeds', methods=['GET'])
-def forward_request():
+def forward_feeds():
     target_url = f"{feed_service_url}/feeds"
     response = requests.get(target_url, params=request.args)
     return (response.text, response.status_code, response.headers.items())
 
+@app.route("/forecast", methods=['GET'])
+def forward_forecast():
+    target_url = f"{forecast_service_url}/forecast"
+    response = requests.get(target_url, params=request.args)
+    return (response.text, response.status_code, response.headers.items())
 
 if __name__ == '__main__':
     app.run(debug=True)
